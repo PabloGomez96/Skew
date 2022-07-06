24e10adfef22308977cafd65949cabf09e318b64
Pablo Gomez <pablo.gomezlp700.4@gmail.com>
2022-07-06 04:47:58 -0600

    Source code commented and Readme file extended

--- README.md
+++ README.md
@@ -5,11 +5,15 @@ Skew is a Rust script whose purpose is to provide its users with a simple, preci
 
 ## :gear: Features
 - Geometric to geopotential altittude conversion will be performed in every calculation automatically
-- 
+- Compute values for Troposphere and Low Stratosphere
+- Dynamic viscosity calcution based on Sutherland's Law
+- Local Speed of Sound calculation based on gases idealization and altitude particular values
+- Thermodynamic properties of gases executed in their polynomial exact form based on absolute temperature
 
 ## :warning: Limitations
 - The results will be presented in aproximated format only with as many significant figures as the author think are correct according to their experience in the field.
 - The calculation methods are valid for Air as ideal gas with a temperature range of [273.15 - 1800]K
+- The calculation methods are valid for Earth atmosphere at meand conditions between [0 - 20,000]m
 
 ## :world_map: Roadmap
 Some options to implement as flags indications:
@@ -21,8 +25,6 @@ Some options to implement as flags indications:
 [Rust Physical constants crate](https://crates.io/crates/physical_constants)
 
 ## :books: Bibliography
-- Yunus cengel for Thermodynamics
-- Intro to flight to speed of sounf
-- Sutherland for viscosity calculation
-- ICAO International Standard Atmosphere mathematical model
-
+- Section 4-4 Internal energy, enthalpy and specific heats of ideal gases, Thermodynamics 8th Edition, Yunus A. Çengel & Michael A. Boles
+- Section 4.9 Speed of Sound, Introduction to Flight 7th Edition, John D. Anderson Jr.
+- ICAO Doc 7488/3, Manual of The The ICAO Standard Atmosphere 3th Edition 
--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,81 @@
 use physical_constants;
 
 //List of pyshical constants
-const BASE_TEMP: f64 = 288.15;        //Kelvin, at MSL
-const BASE_PRESS: f64 = 101325.0;     //Pascals, at MSL
-const BASE_DENS: f64 = 1.225;         //Kilograms per cubic meter, at MSL
-const TROPO_TERM_GRAD: f64 = -6.5e-3; //
-const GRAV: f64 = -1.0 * physical_constants::STANDARD_ACCELERATION_OF_GRAVITY;
-const AIR_IDEAL_CONS: f64 = 287.0;
-const STRATO_TEMP: f64 = 216.66;
-const STRATO_PRESS:f64 = 22552.0;
-const STRATO_DENS:f64 = 0.3629;
-const STRATO_ALTITUDE:f64 = 11000.0;
-const EARTH_RADIUS:f64 = 6356766.0;
-const BASE_VISCOSITY:f64 = 1.7894e-5;
-const SUTHERLAND_TEMP:f64 = 110.0;
-const R_U:f64 = physical_constants::MOLAR_GAS_CONSTANT;
+const BASE_TEMP: f64 = 288.15;         // Kelvin, at MSL
+const BASE_PRESS: f64 = 101325.0;      // pascals, at MSL
+const BASE_DENS: f64 = 1.225;          // kilograms per cubic meter, at MSL
+const TROPO_TERM_GRAD: f64 = -6.5e-3;  // Kelvin/meter, for troposphere only
+const GRAV: f64 = -1.0 * physical_constants::STANDARD_ACCELERATION_OF_GRAVITY; // -9.80665 m/s^2, assuming the negative direction of the force pointing to the earth center 
+const AIR_IDEAL_CONS: f64 = 287.05287; // (Joule/kilogram)/Kelvin, for ideal gas Air 
+const STRATO_TEMP: f64 = 216.66;       // Kelvin, isothermal temperature for stratosphere
+const STRATO_PRESS:f64 = 22552.0;      // pascals, pressure at transition point between tropo and stratosphere =~ 11,000m
+const STRATO_DENS:f64 = 0.3629;        // kilogram/meter, density at transition point between tropo and stratosphere =~ 11,000m
+const STRATO_ALTITUDE:f64 = 11000.0;   // meters
+const EARTH_RADIUS:f64 = 6356766.0;    // meters
+const BASE_VISCOSITY:f64 = 1.7894e-5;  // kilogram/(meter*second), at MSL
+const SUTHERLAND_TEMP:f64 = 110.0;     // Kelvin, Sutherland constant
+const R_U:f64 = physical_constants::MOLAR_GAS_CONSTANT; // Joule/(mol*Kelvin) Ideal molar gas constant
 
+// Geometric to geopotential calculation method
 fn geo_alt(alt: f64) -> f64 {
     let result = alt * (EARTH_RADIUS / (EARTH_RADIUS + alt));
     result
 }
 
+// Troposphere temperature calculation method
 fn temp(alt: f64) -> f64 {
     let result = BASE_TEMP + (TROPO_TERM_GRAD * alt);
     result
 }
 
+// Troposphere pressure calculation method
 fn press(alt: f64) -> f64 {
     let result = BASE_PRESS * f64::powf((BASE_TEMP + (TROPO_TERM_GRAD * alt)) / BASE_TEMP, GRAV / (AIR_IDEAL_CONS * TROPO_TERM_GRAD));
     result
 }
 
+// Troposphere density calculation method
 fn densy(alt: f64) -> f64 {
     let result = BASE_DENS * f64::powf((BASE_TEMP + (TROPO_TERM_GRAD * alt)) / BASE_TEMP, (GRAV / (AIR_IDEAL_CONS * TROPO_TERM_GRAD)) - 1.0);
     result
 }
 
+// Low stratosphere pressure calculation method
 fn strato_press(alt: f64) -> f64 {
     let result = STRATO_PRESS * f64::exp((GRAV / (AIR_IDEAL_CONS * STRATO_TEMP)) * (alt - STRATO_ALTITUDE));
     result
 }
 
+// Low stratosphere density calculation method
 fn strato_densy(alt: f64) -> f64 {
     let result = STRATO_DENS * f64::exp((GRAV / (AIR_IDEAL_CONS * STRATO_TEMP)) * (alt - STRATO_ALTITUDE));
     result
 }
 
+// Temperature based viscosity calculation method
 fn visco(alt: f64) -> f64 {
     let alt_temp = temp(alt);
     let result = BASE_VISCOSITY * ((BASE_TEMP + SUTHERLAND_TEMP) / (alt_temp + SUTHERLAND_TEMP)) * f64::powf(alt_temp / BASE_TEMP, 1.5);
     result
 }
 
+// Constant pressure specific heat for Air calculation method, in polynomial form
 fn ideal_cp(temp: f64) -> f64 {
     let cp = 28.11 + (0.1967e-2 * temp) + (0.4802e-5 * f64::powf(temp, 2.0)) + (-1.966e-9 * f64::powf(temp, 3.0));
     cp
 }
 
+// Constant volume specific heat for Air calculation method 
 fn ideal_cv(temp: f64) -> f64 {
     let cp = ideal_cp(temp);
     let cv = cp - R_U;
     cv
 }
 
+// Temperature based local speed of sound
 fn sound_speed(alt: f64) -> f64 {
     let alt_temp = temp(alt);
-    let gamma = ideal_cp(alt_temp) / ideal_cv(alt_temp);
+    let gamma = ideal_cp(alt_temp) / ideal_cv(alt_temp); // Specific heats ratio
     let c = f64::sqrt(gamma * (press(alt) / densy(alt)));
     c
 }
@@ -76,7 +86,7 @@ fn main() {
     let value: f64 = alt_input.parse().expect("The expected value is a number, not a string!");
     if value < 0.0 {
         panic!("You are under the ground!!!");
-    } else if value > 25000.0 {
+    } else if value > 20000.0 {
         panic!("You are too high for air properties calculation!");
     } else {
         true;
